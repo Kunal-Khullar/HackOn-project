e1935ea977992e0b4d043b8d01fbe5e33778c2fe
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-05-28 23:23:37 +0530

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import myfile from './csvjson.json'
+import {Magic} from 'magic-sdk';
+import {useEffect,useState} from 'react'
+
 
 function App() {
+  // const [data,setData]=useState([]);
+  // const getData=()=>{
+  //   fetch('csvjson.json'
+  //   ,{
+  //     headers : { 
+  //       'Content-Type': 'application/json',
+  //       'Accept': 'application/json'
+  //      }
+  //   }
+  //   )
+  //     .then(function(response){
+  //       console.log(response)
+  //       return response.json();
+  //     })
+  //     .then(function(myJson) {
+  //       console.log(myJson);
+  //       setData(myJson)
+  //     });
+  // }
+  // useEffect(()=>{
+  //   getData()
+  // },[])
+  const data = require('./csvjson.json');
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+      //  data && data.length>0 && data.map((item)=><p>{item.about}</p>)
+     }
     </div>
   );
 }
