5a62a93ddc8f0f2e921b9b6fcfb8c45f96e175f2
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-05-29 18:06:32 +0530

    dashboard

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './dash.css'
-import { Button, Row, Col, Form } from 'react-bootstrap'
+import { Button, Row, Col, Form,Navbar,Nav } from 'react-bootstrap'
 import { Route, HashRouter, NavLink } from 'react-router-dom'
 import Graph from '../options/graph/Graph'
 import Food from '../options/food/Food'
@@ -11,42 +11,33 @@ const Dashboard = () => {
     return (
         <HashRouter>
             <div>
-                <div id='main'>
+            <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Health_Prediction</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavLink exact to="/graph">Graphs</NavLink>
+            <NavLink exact to="/">Food</NavLink>
+            <NavLink exact to="/pred">Prediction</NavLink>
+            
+          </Nav>
+        
+        </Navbar.Collapse>
+      </Navbar>
                     <div className='mid_container'>
                         <Row>
-                            <Col id='left' className='col-md-4'>
-                                <div className='welcome'>
-                                    <h2>Welcome Back</h2>
-                                    <h3>User Name</h3>
-                                </div>
-                                <div className='navs'>
-                                    <NavLink exact to='/'>
-                                        <div className='options'>
-                                            <h4>Graphs</h4>
-                                        </div>
-                                    </NavLink>
-                                    <NavLink exact to='/food'>
-                                        <div className='options'>
-                                            <h4>Food</h4>
-                                        </div>
-                                    </NavLink>
-                                    <NavLink exact to='/pred'>
-                                        <div className='options'>
-                                            <h4>Prediction</h4>
-                                        </div>
-                                    </NavLink>
-                                </div>
-                            </Col>
-                            <Col className='col-md-8'>
+                           
+                            <Col className='col-md-12'>
+                               
                                 <Route exact path="/" component={Food}></Route>
-                                <Route exact path="/food" component={Graph}></Route>
+                                <Route exact path="/graph" component={Graph}></Route>
                                 <Route exact path="/pred" component={Prediction}></Route>
 
                             </Col>
                         </Row>
                     </div>
                 </div>
-            </div>
+          
         </HashRouter>
 
     )
