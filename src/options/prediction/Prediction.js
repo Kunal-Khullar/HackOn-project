148b4b2d008c42347f066d3aaaf0a42bc6f94532
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-05-29 23:48:52 +0530

    prediction

--- src/options/prediction/Prediction.js
+++ src/options/prediction/Prediction.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import './prediction.css'
+import {Row,Col,Button} from 'react-bootstrap'
 const Prediction = () => {
     return (
         <div>
-            hi
+           <div className="bot">
+               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+               <Button>PREDICT</Button>
+           </div>
         </div>
     )
 }
