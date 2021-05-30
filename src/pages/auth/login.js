4b59a40ae17b2cccc31125d1a52b862d7b6780f8
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-05-30 08:41:11 +0530

    gg

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -69,15 +69,7 @@ const Login = () => {
           </Button>
 
         </div>
-        <div className="google" id="g2">
-          <Button className="goog" onClick={() => fblog()}>
-            <img
-              className="googe"
-              src="https://img.icons8.com/fluent/48/000000/facebook-new.png" />
-
-            Sign In With Facebook
-          </Button>
-        </div>
+       it 
         <h4>OR</h4>
         <div className="cred">
           <Form>
